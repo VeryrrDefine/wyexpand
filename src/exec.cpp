3aba2fef415b392ee1a1b6e5336effb512fb3aff
VeryrrDefine <1308061599@qq.com>
2026-02-02 14:43:47 +0800

    add output s & p

--- src/exec.cpp
+++ src/exec.cpp
@@ -7,7 +7,7 @@
 #include "wyexpand/header.h"
 using namespace wyexpand;
 
-std::string seq_to_string(std::vector<int> seq)
+std::string seq_to_string(vecint seq)
 {
     std::string result = "";
     for (auto it = seq.begin(); it != seq.end(); ++it)
@@ -23,11 +23,22 @@ std::string seq_to_string(std::vector<int> seq)
 
 int main()
 {
-    std::vector<int> a{1, 2};
+    vecint a{1, 2};
     Expander expander(a);
     std::cout << "Initial Sequence: " << seq_to_string(expander.initialSeq) << std::endl;
-    std::vector<int> result(expander.startExpand());
+    vecint result(expander.startExpand());
     std::cout << "Result: " << seq_to_string(result) << std::endl;
-    std::cout << "Row 1 Parents: " << seq_to_string(expander.parents[{0}]) << std::endl;
+    vecint level({0});
+    while (true)
+    {
+        auto seq = expander.rows[level];
+        auto parents = expander.parents[level];
+        if (seq.empty())
+            break;
+
+        std::cout << "Row " + seq_to_string(level) + " S" << seq_to_string(seq) << "\t\t";
+        std::cout << "P" << seq_to_string(parents) << std::endl;
+        level = succ(level);
+    }
     return 0;
 }
